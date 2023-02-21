04d8f1103569fd703c93aac65a62cbd2dd6f2484
Manavvora <mkvora2@illinois.edu>
2023-02-20 19:45:20 -0600

    Updated PI

--- train_gridworld.py
+++ train_gridworld.py
@@ -11,37 +11,74 @@ def policy_iteration():
     num_states = 25
     num_actions = 4
     V = np.zeros(num_states)
-   
-    pi = np.zeros(num_states)
+    pi = np.ones([num_states,num_actions])/num_actions
     # pi_new = np.zeros(num_states)
     theta = 1e-6
     gamma = 0.95
-    #delta = 1
+    log = {
+        't': [0],
+        's': [],
+        'a': [],
+        'r': [],
+        'V': [],
+        'iters': []
+    }
     while True:
+        iters = 0
         delta = np.inf
         while delta > theta:
+            # V = np.zeros(num_states)
             delta = 0
+            iters += 1
+            print("New iteration")
             for s in range(num_states):
-                V_new = np.zeros(num_states)
-                for s_new in range(num_states):
-                    V_new[s] += env.p(s_new,s,pi[s])*(env.r(s,pi[s]) + gamma*V[s_new])
-                delta = max(delta, np.abs(V[s]-V_new[s]))
+                v = 0
+                for act,action_prob in enumerate(pi[s]):
+                    for s_new in range(num_states):
+                        v += env.p(s_new,s,act)*action_prob*(env.r(s,act) + gamma*V[s_new])
+                delta = max(delta, np.abs(v-V[s]))
+                V[s] = v
                 print(delta)
-            V = V_new
+            print('------------')
+        log['V'].append(np.mean(V))
+        log['iters'].append(iters)
         policy_stable = True
         for s in range(num_states):
-            chosen_a = pi[s]
+            chosen_a = np.argmax(pi[s])
             pi_new = np.zeros(num_actions)
             for a in range(num_actions):
                 for s_new in range(num_states):
                     pi_new[a] += env.p(s_new,s,a)*(env.r(s,a) + gamma*V[s_new])
-                # if maximum < one_step:
-                #     maximum = one_step
-                #     best_a = a
-            pi[s] = np.argmax(pi_new)
-            if chosen_a != pi[s]:
+            best_a = np.argmax(pi_new)
+            if chosen_a != best_a:
                 policy_stable = False
+            pi[s] = np.eye(num_actions)[best_a]
         if policy_stable:
-            return V, pi
-            
-V1, pi1 = policy_iteration()
+            return V, pi, log
+
+def main():    
+    V1, pi1, log = policy_iteration()
+    print(V1)
+    print("------------------------")
+    print(pi1)
+    # sp = env.reset()
+    # log['s'].append(sp)
+    # done = False
+    # while not done:
+    #     a = pi1[sp]
+    #     (sp, rp, done) = env.step(a)
+    #     log['t'].append(log['t'][-1] + 1)
+    #     log['s'].append(sp)
+    #     log['a'].append(a)
+    #     log['r'].append(rp)
+
+    # # Plot data and save to png file
+    # # plt.plot(log['t'], log['s'])
+    # #plt.plot(log['t'][:-1], log['a'])
+    plt.plot(log['iters'], log['V'])
+    # # plt.plot(log['t'][:-1], log['r'])
+    # plt.legend(['s', 'a', 'V', 'r'])
+    plt.show()
+
+if __name__ == '__main__':
+    main()
